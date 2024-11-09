719ffa9cbc6fa6e3af15706e0e38c9f173644b52
Carlos Jesús Ordaz Hoyos <carlos.ordaz@acomo.com.pe>
2024-11-08 20:48:42 -0500

    xx

--- src/main/java/com/example/demo/IAM/domain/model/entities/User.java
+++ src/main/java/com/example/demo/IAM/domain/model/entities/User.java
@@ -33,7 +33,7 @@ public class User {
     private String genre;
     private String typeContract;
 
-    //enterprice
+    //enterprice.
     private String businessName;
     private String businessType;
     private String businessLocation;
